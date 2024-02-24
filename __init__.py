0f1f6de7b227bd9ac7f8e7a42aa89cf6f0138008
CapsAdmin <eliashogstvedt@gmail.com>
2024-02-24 16:18:19 +0000

    attempt to optimize and tweak, still doesn't seem to be performant or good enough quality compared to tome

--- __init__.py
+++ __init__.py
@@ -7,39 +7,46 @@ class ToDoPatchModel:
     def INPUT_TYPES(s):
         return {"required": { 
             "model": ("MODEL",),
-            "ratio": ("FLOAT", {"default": 0.3, "min": 0.0, "max": 1.0, "step": 0.01}),
+            "scale_factor": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 10, "step": 0.01}),
+            "sqrt_scale_factor": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 10000000, "step": 0.01}),
+            "resize_mode": (['nearest', 'linear', 'area', 'nearest-exact'], {"default": "nearest"}),
+            "use_downscaled_kv": ("BOOLEAN", {"default": True}),
         }}
     RETURN_TYPES = ("MODEL",)
     FUNCTION = "patch"
 
     CATEGORY = "_for_testing"
 
-    def patch(self, model, ratio):
-        if ratio == 1:
-            return (model, )
-        
-        ratio_sqr = math.sqrt(ratio)
+    def patch(self, model, scale_factor, sqrt_scale_factor, resize_mode, use_downscaled_kv):
 
-        @torch.inference_mode() # idk if this helps
-        def D(x):
-            # x is of shape [batch_size, num_keys, key_dim]
-
-            x = x.permute(0, 2, 1)  # Change to [batch_size, key_dim, num_keys]
-            x = F.interpolate(x, scale_factor=ratio, mode='nearest')
-            x = x.permute(0, 2, 1)  # Change back
-
-            return x
+        sqrt_scale_factor = math.sqrt(sqrt_scale_factor)
         
         @torch.inference_mode()
         def todo_patch(q, k, v, extra_options):
-            # Compute scaled dot-product of Q and D(K)^T
-            q = torch.matmul(q, D(k).transpose(-2, -1)) / ratio_sqr
+            orig_k = k
+            orig_v = v
+            # q and k is of shape [batch_size, num_keys, key_dim]
 
-            # Apply softmax to scaled dot-product
+            # change to [batch_size, key_dim, num_keys] but keep as is to avoid redundant double transpose
+            k = F.interpolate(v.transpose(-2, -1), scale_factor=scale_factor, mode=resize_mode)
+
+            # change to [batch_size, key_dim, num_keys] and change back
+            v = F.interpolate(v.transpose(-2, -1), scale_factor=scale_factor, mode=resize_mode).transpose(-2, -1)
+
+            # compute scaled dot-product of Q and D(K)^T
+            q = torch.matmul(q, k) / sqrt_scale_factor
+            
+            # apply softmax to scaled dot-product
             q = F.softmax(q, dim=-1)
 
-            # Multiply attention scores with D(V)
-            q = torch.matmul(q, D(v))
+            # multiply attention scores with D(V)
+            q = torch.matmul(q, v)
+
+            if not use_downscaled_kv:
+                return q, orig_k, orig_v
+
+            # change k back to [batch_size, num_keys, key_dim]
+            k = k.transpose(-2, -1)
 
             return q, k, v
         
