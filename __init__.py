ee88ff7e4217cc9d97c70fa86508976257346b9d
CapsAdmin <eliashogstvedt@gmail.com>
2024-02-25 16:32:01 +0000

    sort of get it working

--- __init__.py
+++ __init__.py
@@ -1,57 +1,50 @@
 import torch
 import math
 import torch.nn.functional as F
+from comfy.ldm.modules.attention import optimized_attention_for_device
 
 class ToDoPatchModel:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": { 
             "model": ("MODEL",),
-            "scale_factor": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 10, "step": 0.01}),
-            "sqrt_scale_factor": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 10000000, "step": 0.01}),
+            "downscale_factor": ("INT", {"default": 8, "min": 1, "max": 128}),
             "resize_mode": (['nearest', 'linear', 'area', 'nearest-exact'], {"default": "nearest"}),
-            "use_downscaled_kv": ("BOOLEAN", {"default": True}),
+            "downscale_input": ("BOOLEAN", {"default": True}),
+            "downscale_middle": ("BOOLEAN", {"default": True}),
+            "downscale_output": ("BOOLEAN", {"default": True}),
         }}
     RETURN_TYPES = ("MODEL",)
     FUNCTION = "patch"
 
     CATEGORY = "_for_testing"
 
-    def patch(self, model, scale_factor, sqrt_scale_factor, resize_mode, use_downscaled_kv):
+    def patch(self, model, downscale_factor, resize_mode, downscale_input, downscale_middle, downscale_output):
+        scale = 1/downscale_factor
+        def downsample(x, scale_factor, resize_mode):
+            x = x.transpose(-2, -1)
+            x = F.interpolate(x, scale_factor=scale_factor, mode=resize_mode)
+            x = x.transpose(-2, -1)
+            return x
 
-        sqrt_scale_factor = math.sqrt(sqrt_scale_factor)
-        
         @torch.inference_mode()
-        def todo_patch(q, k, v, extra_options):
-            orig_k = k
-            orig_v = v
-            # q and k is of shape [batch_size, num_keys, key_dim]
+        def todo(q, k, v, extra_options):
+            if extra_options["block"][0] == "input" and not downscale_input: return q,k,v
+            if extra_options["block"][0] == "middle" and not downscale_middle: return q,k,v
+            if extra_options["block"][0] == "output" and not downscale_output: return q,k,v
 
-            # change to [batch_size, key_dim, num_keys] but keep as is to avoid redundant double transpose
-            k = F.interpolate(v.transpose(-2, -1), scale_factor=scale_factor, mode=resize_mode)
+            k = downsample(k, scale, resize_mode)
+            v = downsample(v, scale, resize_mode)
 
-            # change to [batch_size, key_dim, num_keys] and change back
-            v = F.interpolate(v.transpose(-2, -1), scale_factor=scale_factor, mode=resize_mode).transpose(-2, -1)
-
-            # compute scaled dot-product of Q and D(K)^T
-            q = torch.matmul(q, k) / sqrt_scale_factor
-            
-            # apply softmax to scaled dot-product
-            q = F.softmax(q, dim=-1)
-
-            # multiply attention scores with D(V)
-            q = torch.matmul(q, v)
-
-            if not use_downscaled_kv:
-                return q, orig_k, orig_v
-
-            # change k back to [batch_size, num_keys, key_dim]
-            k = k.transpose(-2, -1)
+            # TODO, we still need to multiply K by "1/sqrt(dk)" in the attention function which is done later
+            # it was suggested by comfyanonymous that we could just scale K by a constant, but in my experience
+            # this did not work very well.
+            # either I don't know what dk is or it doesn't work
 
             return q, k, v
-        
+
         m = model.clone()
-        m.set_model_attn1_patch(todo_patch)
+        m.set_model_attn1_patch(todo)
         return (m, )
     
 NODE_CLASS_MAPPINGS = {
